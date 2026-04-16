4dcd215d896208a2e637b7caac927def62eee338
veragrosskop <veragrosskop@gmail.com>
2026-04-16 18:36:40 +0200

    Added Promotions

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from unittest import case
 
 import store
 import products
+import promotions
 
 
 def ask_int_input(min_choices: int, max_choices: int):
@@ -122,6 +123,16 @@ if __name__ == "__main__":
         products.NonStockedProduct("Windows License", price=125),
         products.LimitedProduct("Shipping", price=10, quantity=250, maximum=1),
     ]
+
+    # Create promotion catalog
+    second_half_price = promotions.SecondHalfPrice("Second Half price!")
+    third_one_free = promotions.ThirdOneFree("Third One Free!")
+    thirty_percent = promotions.PercentDiscount("30% off!", percent=30)
+
+    # Add promotions to products
+    product_list[0].set_promotion(second_half_price)
+    product_list[1].set_promotion(third_one_free)
+    product_list[3].set_promotion(thirty_percent)
     best_buy = store.Store(product_list)
 
     start(best_buy)
---
+++ promotions.py
@@ -0,0 +1,66 @@
+import products
+from typing import Optional
+from abc import ABC, abstractmethod
+
+
+class Promotion(ABC):
+    """This class creates a promotion for a product."""
+
+    def __init__(self, name: str):
+        self.name = name
+
+    @abstractmethod
+    def apply_promotion(
+        self, product: products.Product, quantity: int, percentage: Optional[float]
+    ) -> float:
+        """Applies a promotion to the product and returns the discounted price."""
+        pass
+
+
+class PercentageDiscount(Promotion):
+    """This class creates a percentage discount for a product."""
+
+    def __init__(self, name: str):
+        super().__init__(name=name)
+
+    def apply_promotion(self, product: products.Product, quantity: int, percentage: float) -> float:
+        """Applies a promotion to the product and returns the discounted price."""
+
+        discounted_price = product.price * quantity * ((100 - percentage) / 100)
+        return discounted_price
+
+
+class SecondHalfPriceDiscount(Promotion):
+    """This class reduces the price for every second product by half."""
+
+    def __init__(self, name: str):
+        super().__init__(name=name)
+
+    def apply_promotion(self, product: products.Product, quantity: int) -> float:
+        """Applies a promotion to the product and returns the discounted price."""
+
+        non_discounted_amount = quantity % 2
+        discounted_amount = quantity - non_discounted_amount
+        discounted_price = (product.price * 0.5 * (discounted_amount / 2)) + (
+            non_discounted_amount * product.price * (discounted_amount / 2)
+        )
+
+        return discounted_price
+
+
+class ThirdOneFree(Promotion):
+    """This class creates a buy two get one free product promotion."""
+
+    def __init__(self, name: str):
+        super().__init__(name=name)
+
+    def apply_promotion(self, product: products.Product, quantity: int) -> float:
+        """Applies a promotion to the product and returns the discounted price."""
+
+        non_discounted_amount = quantity % 3
+        discounted_amount = quantity - non_discounted_amount
+        discounted_price = (product.price * ((2 * discounted_amount) / 3)) + (
+            non_discounted_amount * product.price
+        )
+
+        return discounted_price
